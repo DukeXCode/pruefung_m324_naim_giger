83e2b1357c1786c86ca66351cf7b3d80b7ecfceb
DukeXCode <naim.giger@outlook.com>
2024-12-12 09:35:11 +0100

    format with this cofig

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,12 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
     languageOptions: { globals: globals.browser },
     rules: {
-      eqeqeq: ["error", "always"],
+      eqeqeq: ['error', 'always'],
     },
   },
   pluginJs.configs.recommended,
